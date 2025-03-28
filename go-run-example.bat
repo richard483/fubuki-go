dfbd77ee08d84a8a7fbfc108532785578600d0ee
richard483 <richard.william483@gmail.com>
2025-03-29 01:20:44 +0700

    config: added .bat runner for windows development purposes

---
+++ go-run-example.bat
@@ -0,0 +1,9 @@
+set GEMINI_API_KEY=example-value
+set POSTGRES_URI=example-value
+set GEMINI_API=example-value
+set GOOGLE_PROJECT_ID=example-value
+set HOST=example-value
+set GOOGLE_ACCESS_TOKEN=example-value
+set RETRIEVE_HISTORY=example-value
+set RELEASE_MODE=example-value
+go run main.go
